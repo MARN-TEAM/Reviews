591e4a782b58819f0f0312cedad94bb0976c195c
achrefthameur <benthameur@mail.com>
2021-03-24 15:43:48 +0100

    rating style part

--- service/client/components/Reviews.jsx
+++ service/client/components/Reviews.jsx
@@ -10,12 +10,72 @@ class Reviews extends React.Component{
         return(
             <div className="container">
                 <h2 style={{fontSize:'20px',fontWeight:'100'}}>Rating & Reviews</h2>
-                    <div className="row">
+                <div id='Reviews-data-container'>
+                <div className="row">
                             <div className="col col-xl-4">
                             <div id='Rating-tab-Reviews'>
                                 <div className='row'>
-                                        <span style={{fontSize:'50px'}}>3.5</span>
+                                   <div className='col-3'><span style={{fontSize:'60px',fontWeight:'600'}}>3.5</span> </div> 
+                                   <div className='col-6 Stars-mini-tab-rating' ><span><i className="fas fa-star "></i> </span><i className="fas fa-star"></i> <i className="fas fa-star Colored-Star-rating"></i> <i className="far fa-star"></i> <i className="far fa-star"></i></div>
                                 </div>
+                                <div className='row'>
+                                        <p id='rating-perceentage-para'>100 % of reviews recommend this product</p>
+                                </div>
+                                <div className='row'>
+                                    <button type="button" className="btn btn-link Start-button-rating-tab">5 stars </button><div className='Stars-percentage-bar' id='Stars-percentage-bar-5'></div>
+                                </div>
+                                <div className='row'>
+                                    <button type="button" className="btn btn-link Start-button-rating-tab">4 stars </button><div className='Stars-percentage-bar' id='Stars-percentage-bar-4'></div>
+                                </div>
+                                <div className='row'>
+                                    <button type="button" className="btn btn-link Start-button-rating-tab">3 stars </button><div className='Stars-percentage-bar' id='Stars-percentage-bar-3'></div>
+                                </div>
+                                <div className='row'>
+                                    <button type="button" className="btn btn-link Start-button-rating-tab">2 stars </button><div className='Stars-percentage-bar' id='Stars-percentage-bar-2'></div>
+                                </div>
+                                <div className='row'>
+                                    <button type="button" className="btn btn-link Start-button-rating-tab">1 stars </button><div className='Stars-percentage-bar' id='Stars-percentage-bar-1'></div>
+                                </div>
+                                <div className='row size-cursor-rate'>
+                                    Size
+                                    </div>
+                                    <div className='row'>
+                                    <span className='bar-for-cursor'></span><div className='bar-for-cursor'></div><div className='bar-for-cursor'></div>
+
+                                    </div>
+                                    <div className='row size-scrollar-bar-container'>
+                                        <div className='col'>
+                                             <span className='size-caract-font' style={{marginLeft:'-8px'}}>Too small</span>
+                                        </div>
+                                        <div className='col'>
+                                            <span className='size-caract-font'  >Perfect</span>
+                                        </div>
+                                        <div className='col'>
+                                            <span className='size-caract-font' style={{marginLeft:'10px'}}>Too Large</span>
+                                        </div>
+                                    
+                                    
+                                    </div>
+                                <div className='row size-cursor-rate'>
+                                    Comfort
+                                    </div>
+                                    <div className='row'>
+                                    <span className='bar-for-cursor'></span><div className='bar-for-cursor'></div><div className='bar-for-cursor'></div>
+
+                                    </div>
+                                    <div className='row size-scrollar-bar-container'>
+                                        <div className='col'>
+                                             <span className='size-caract-font' style={{marginLeft:'-8px'}}>Poor</span>
+                                        </div>
+                                        <div className='col'>
+                                            <span className='size-caract-font'  ></span>
+                                        </div>
+                                        <div className='col'>
+                                            <span className='size-caract-font' style={{marginLeft:'18px'}}>Perfect</span>
+                                        </div>
+                                    
+                                    
+                                    </div>
                                 </div>
                             </div>
                             <div className="col">
@@ -31,6 +91,7 @@ class Reviews extends React.Component{
                                     
                             </div>
                     </div>
+                </div>
             </div>
         )
     }
